d537d3879160d2a0abd9ab48ed51d1ee16414c05
khoffan <khoffan222@gmail.com>
2022-11-22 22:48:33 +0700

    requirements

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-###chatbot
+# chatbot
+### วิธีรันคำสั่งในการรัยตัว chatbot 
+
+` python <filename> `
+### เวลาออกจากตัว chatbot ให้พิมคำว่า **quit**
+### สามารถเพิ่มข้อความได้ที่ในส่วนของ response
--- chatbot.py
+++ chatbot.py
@@ -29,7 +29,7 @@ def check_all_massage(massage):
         hight_porb_masg[bot_res] = mmassge_prob(massage,list_word,single_response,require_word)
     
     #response ----------------------------------------
-
+    #เพิ่มค่าข้อความได้ตามต้องการได้
     response("hello",['hello','hi','whaddub'],single_response=True)
     response("i\'m find",['how','are','you','doing'],require_word=['how'])
     response("hey i am lucy",['what','your','name'],require_word=['what','name'])
